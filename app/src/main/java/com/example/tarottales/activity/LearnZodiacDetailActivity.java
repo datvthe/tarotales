dc3a4c40de43f30cbf2918796ed35eb8ffc456cd
tqnhat2358 <taquangnhat23a@gmail.com>
2025-11-07 06:36:00 +0700

    Done History, fix bug

--- app/src/main/java/com/example/tarottales/activity/LearnZodiacDetailActivity.java
+++ app/src/main/java/com/example/tarottales/activity/LearnZodiacDetailActivity.java
@@ -13,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.tarottales.Database.LearnDAO;
 import com.example.tarottales.Model.TarotCard;
 import com.example.tarottales.Model.Zodiac;
 import com.example.tarottales.R;
